3b541ec8a5696bffcbde8108ffe6b3323d76a57c
takeshi.inagaki <inatake@gmail.com>
2019-03-03 06:44:49 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+#before_action :correct_user, only: [:destroy  ]
+
+  before_action :correct_user, only: [:destroy ,:update, :edit ]
 
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
@@ -52,21 +54,24 @@ class TasksController < ApplicationController
   end
 
 
-  def correct_user
-    @task = current_user.tasks.find_by(id: params[:id])
-    unless @task
-      redirect_to root_url
-    end
-  end
+
 
   
   private
+  
   def task_params
     params.require(:task).permit(:content, :status)
   end
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
 
